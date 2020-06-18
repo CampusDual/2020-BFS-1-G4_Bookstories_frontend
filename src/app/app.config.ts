1afcb4650606ca35081f41149a86eaa8be719342
PabloAlonso-im <pablo.alonso@campusdual.com>
2020-06-18 11:27:29 +0200

    translation of books

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,7 +12,7 @@ export const CONFIG: Config = {
   // Application identifier. Is the unique package identifier of the app.
   // It is used when storing or managing temporal data related with the app.
   // By default is set as 'ontimize-web-uuid'.
-  uuid: 'com.imatia.web.bookstoriesApi',
+  uuid: 'com.ontimize.bookstories',
 
   // Title of the app
   title: 'BookStories',
