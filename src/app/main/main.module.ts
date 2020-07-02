a458bed804f4112552db0edd9c9da43b79922349
PabloAlonso-im <pablo.alonso@campusdual.com>
2020-07-02 17:57:30 +0200

    first starrate

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -5,6 +5,7 @@ import { MainComponent } from './main.component';
 import { MainRoutingModule } from './main-routing.module';
 
 import { BooksMiniatureComponent } from '../shared/books-miniature/books-miniature.component';
+import { StarRatingComponent } from 'app/shared/star-rating/star-rating.component';
 
 
 @NgModule({
@@ -22,6 +23,6 @@ import { BooksMiniatureComponent } from '../shared/books-miniature/books-miniatu
     
     
     
-  ],exports: [BooksMiniatureComponent]
+  ],exports: [BooksMiniatureComponent,StarRatingComponent]
 })
 export class MainModule { }
