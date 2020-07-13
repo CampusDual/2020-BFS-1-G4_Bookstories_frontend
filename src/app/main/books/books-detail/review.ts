dfae32ad672bdfd52d7d2e5ec0043aab47d2c93f
EmilioMendezD <emilio.mendez@campusdual.com>
2020-07-13 10:20:03 +0200

    author hover and format code

--- src/app/main/books/books-detail/review.ts
+++ src/app/main/books/books-detail/review.ts
@@ -1,8 +1,8 @@
 export interface Review {
-  REVIEW_ID: number;
-  VALUE?: number;
-  COMMENT?: string;
-  REVIEW_DATE: Date;
-  BOOKS_BOOK_ID: number;
-  TUSERS_USER: string;
+    REVIEW_ID: number;
+    VALUE?: number;
+    COMMENT?: string;
+    REVIEW_DATE: Date;
+    BOOKS_BOOK_ID: number;
+    TUSERS_USER: string;
 }
